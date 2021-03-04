48997fecd907ab36dbe0e47de74a09030cc5cd59
xwc <1877169718@qq.com>
2021-03-04 19:40:22 +0800

    modify hello.py shazi

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 print('hello world')
 num = int(input('请输入一个整数:'))
-for i in range(2,num):
-    if num%i==0:
-    	break
+for i in range(2, num):
+    if num % i == 0:
+        break
 else:
-    print(num,'是质数...')
+    print(num, '是质数...')
+    print('shazi')
